19df8ece2b89286f2cd8f3087da97b2c4599ae99
matteo <matteoezio@me.com>
2020-12-01 16:33:58 +0100

    ho inserito delle funzioni per generare i numeri randomici
    successivamente ho lavorato sull'inserimento dei numeri da parte dell'utente (va ancora migliorato questo)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,39 @@
 // * Pensate prima in italiano.
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzae il programma.
+
+$(document).ready(function(){
+
+    var genericNumberArray = [];
+    while(genericNumberArray.length < 5){
+        var randomNumber = Math.floor(Math.random() * 5) + 1;
+        if (genericNumberArray.indexOf(randomNumber) === -1) {
+            genericNumberArray.push(randomNumber)
+        }
+    }
+    alert(genericNumberArray);
+
+    
+    var userValue = [];
+    while(userValue.length < 5){
+        var newValue = prompt("inserisci i numeri che hai appena visto");
+        if (newValue === 0) {
+            alert('il numero zero non è ammesso, scegline uno nuovo');
+        } else if (userValue.indexOf(newValue) === -1) {
+            userValue.push(newValue)
+        }
+    }
+
+    var genericValue = [];
+    function checkerTypeOne(randomArray, genericValue) {
+        var index = 0;
+        while (index < randomArray.length) {
+            if (genericValue === randomArray[index]) {
+                return true;
+            }
+            index++;
+        }
+    }
+
+    checkerTypeOne(genericNumberArray, userValue);
+});

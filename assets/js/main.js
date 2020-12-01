5709493167b08ebf9444c39dacd3632a7de82e63
matteo <matteoezio@me.com>
2020-12-01 19:10:06 +0100

    ho fatto un enorme refactoring del codice iniziale

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@
 // * Individuate gli elementi di cui avete bisogno per realizzae il programma.
 
 $(document).ready(function(){
-
     var genericNumberArray = [];
     while(genericNumberArray.length < 5){
         var randomNumber = Math.floor(Math.random() * 5) + 1;
@@ -15,29 +14,30 @@ $(document).ready(function(){
             genericNumberArray.push(randomNumber)
         }
     }
-    alert(genericNumberArray);
+    // alert(genericNumberArray);
+    console.log(genericNumberArray);
 
-    
-    var userValue = [];
-    while(userValue.length < 5){
-        var newValue = prompt("inserisci i numeri che hai appena visto");
-        if (newValue === 0) {
-            alert('il numero zero non è ammesso, scegline uno nuovo');
-        } else if (userValue.indexOf(newValue) === -1) {
-            userValue.push(newValue)
-        }
-    }
+    function userMemoryTest() {
 
-    var genericValue = [];
-    function checkerTypeOne(randomArray, genericValue) {
-        var index = 0;
-        while (index < randomArray.length) {
-            if (genericValue === randomArray[index]) {
-                return true;
+        var rememberedNumber = [];
+        var userValue = [];
+        while(userValue.length < 5){
+            var newValue = Number(prompt("inserisci i numeri che hai appena visto"));
+            if (userValue.indexOf(newValue) === -1) {
+                userValue.push(newValue);
+                index = 0;
+                while (index < 5) {
+                    if (newValue === genericNumberArray[index]){
+                        rememberedNumber.push(newValue);
+                    } else if (newValue !== genericNumberArray[index]){
+                        console.log('mancato');
+                    }
+                    index++;
+                }
             }
-            index++;
         }
+        console.log(userValue);
+        console.log('complimenti hai ricordato questi numeri = ' + rememberedNumber);
     }
-
-    checkerTypeOne(genericNumberArray, userValue);
+    userMemoryTest();
 });

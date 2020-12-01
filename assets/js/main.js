a1eb1625c24aee66cd8353460cc7c63d1adb80b3
matteo <matteoezio@me.com>
2020-12-01 20:19:59 +0100

    completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,14 @@
-// Un alert espone 5 numeri casuali diversi.
-// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
-// Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzae il programma.
-
 $(document).ready(function(){
     var genericNumberArray = [];
     while(genericNumberArray.length < 5){
-        var randomNumber = Math.floor(Math.random() * 5) + 1;
+        var randomNumber = Math.floor(Math.random() * 100) + 1;
         if (genericNumberArray.indexOf(randomNumber) === -1) {
             genericNumberArray.push(randomNumber)
         }
     }
-    // alert(genericNumberArray);
-    console.log(genericNumberArray);
+    alert(genericNumberArray);
 
     function userMemoryTest() {
-
         var rememberedNumber = [];
         var userValue = [];
         while(userValue.length < 5){
@@ -29,15 +19,25 @@ $(document).ready(function(){
                 while (index < 5) {
                     if (newValue === genericNumberArray[index]){
                         rememberedNumber.push(newValue);
-                    } else if (newValue !== genericNumberArray[index]){
-                        console.log('mancato');
                     }
                     index++;
                 }
             }
         }
-        console.log(userValue);
-        console.log('complimenti hai ricordato questi numeri = ' + rememberedNumber);
+        if (rememberedNumber.length > 0) {
+            alert('Questi sono i numeri che hai ricordato => ' + rememberedNumber + '.' + " Questo era l' elenco completo =>" + genericNumberArray);
+        } else {
+            alert("Non hai ricordato alcun numero. Questo era l'elenco completo =>" + genericNumberArray);
+        }
     }
-    userMemoryTest();
+
+    var seconds = 30;
+    var interval = setInterval(function(){
+        if (seconds ===0) {
+            userMemoryTest();
+            clearInterval(interval);
+        } else {
+            seconds--;
+        }
+    }, 1000);
 });
